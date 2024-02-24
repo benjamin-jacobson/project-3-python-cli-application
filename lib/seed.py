c0d755c00029067dd593efa8ce9f32b0e0ee1aaa
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-02-23 22:55:16 -0500

    successfully created user table in db via seeding, and class method create_table

--- lib/seed.py
+++ lib/seed.py
@@ -3,4 +3,9 @@
 from classes.user import User
 from classes.__init__ import CONN, CURSOR
 
-print("hello")
+def seed_database():
+    User.create_table()
+
+
+seed_database()
+print("Seeded database")
